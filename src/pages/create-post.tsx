a9859a8d60654ea272da10aafc653489908abf09
vendredl <node.js_@kakao.com>
2023-11-18 20:29:02 +0900

    :speech_balloon: Drag & Drop시 cloudflare의 이미지 링크를 사용하여 markdown으로 변환하도록 추가

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { marked } from 'marked';
 import DragAndDropUpload from './components/DragAndDrop';
@@ -11,23 +11,35 @@ marked.setOptions({
   });
 
 export default function PostEditor() {
-  const { register, watch } = useForm();
-  const markdown = watch('markdown');
+  const { register, watch, setValue } = useForm();
+  const [imageVariants, setImageVariants] = useState("");
+  let markdown = watch('markdown');
+
+  const handleImageUpload = (variants: any) => {
+    setImageVariants(variants);
+    // 여기서 imageVariants 상태를 다룰 수 있습니다.
+    // markdown = `${markdown} <img src='${variants}'></img>`
+    console.log(`데이터 받기 성공..! | ${variants}`)
+    const imageMarkdown = `![image](${variants})\n`;
+
+    // 현재 마크다운 텍스트에 이미지 마크다운 추가
+    setValue('markdown', markdown + imageMarkdown);
+  };
 
   return (
-    <div className="flex">
-      <div className="flex-1">
+    <div className="flex overflow-hidden h-screen -m-6">
+      <div className="flex-1 h-screen">
         <form>
           <textarea
             {...register('markdown')}
-            className="w-full h-screen p-8"
+            className="w-full h-screen p-8 overflow-scroll"
           />
         </form>
-        <DragAndDropUpload />
+        <DragAndDropUpload onImageUpload={handleImageUpload} />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 h-screen">
         <div
-          className="prose p-8"
+          className="prose p-8 overflow-scroll h-full flex-rows justify-start"
           dangerouslySetInnerHTML={{ __html: marked.parse(markdown || '') }}
         />
         {/* <div>{marked.parse(markdown || '')}</div> */}

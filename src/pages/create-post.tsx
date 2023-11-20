dd98c7db8864780422d68a00d7f5a87db175e32f
vendredl <node.js_@kakao.com>
2023-11-20 18:16:26 +0900

    :lipstick: Input(title, type) 추가

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -17,19 +17,27 @@ export default function PostEditor() {
 
   const handleImageUpload = (variants: any) => {
     setImageVariants(variants);
-    // 여기서 imageVariants 상태를 다룰 수 있습니다.
-    // markdown = `${markdown} <img src='${variants}'></img>`
+
     console.log(`데이터 받기 성공..! | ${variants}`)
     const imageMarkdown = `![image](${variants})\n`;
 
-    // 현재 마크다운 텍스트에 이미지 마크다운 추가
     setValue('markdown', markdown + imageMarkdown);
   };
 
   return (
-    <div className="flex overflow-hidden h-screen -m-6">
-      <div className="flex-1 h-screen">
+    <div className="flex overflow-hidden h-screen -m-6"> 
+      <div className="flex-1 h-screen w-1/2 bg-yellow-100">
         <form>
+          <input 
+            {...register('title')}
+            placeholder='Title'
+            className="w-full h-12 p-8 overflow-scroll bg-blue-100"
+          />
+          <input 
+            {...register('type')}
+            placeholder='Type'
+            className="w-full h-12 p-8 overflow-scroll bg-orange-100"
+          />
           <textarea
             {...register('markdown')}
             className="w-full h-screen p-8 overflow-scroll"
@@ -37,7 +45,7 @@ export default function PostEditor() {
         </form>
         <DragAndDropUpload onImageUpload={handleImageUpload} />
       </div>
-      <div className="flex-1 h-screen">
+      <div className="flex-1 h-screen w-1/2 bg-green-100">
         <div
           className="prose p-8 overflow-scroll h-full flex-rows justify-start"
           dangerouslySetInnerHTML={{ __html: marked.parse(markdown || '') }}

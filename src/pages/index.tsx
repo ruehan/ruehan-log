e0b822462ac8d1bd4fb9cede5072f59aee8db5d4
vendredl <node.js_@kakao.com>
2023-11-22 18:45:50 +0900

    :lipstick: Filter와 목차 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,62 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import { marked } from 'marked';
+
+const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const { data: posts, error } = useSWR('/api/get-post', fetcher);
+  const [selectedType, setSelectedType] = useState('테스트');
 
   useEffect(() => {
-    // 로그인 상태가 "loading"이 아니면서 세션이 없는 경우
+    console.log(session)
     if (status !== 'loading' && !session) {
-      // 로그인 페이지로 리디렉션
-      signIn();
+      // signIn();
     }
   }, [session, status, router]);
 
-  // 로그인 상태가 로딩 중이거나 세션이 있는 경우, 페이지 내용 표시
   if (status === 'loading' || !session) {
     return <div>Loading...</div>;
   }
 
+  if(!posts){
+    return <div>Loading...</div>
+  }
+
+  console.log(posts)
+
+  const types = new Set(posts.getPost.map((post: any) => post.type));
+  const filteredPosts = posts.getPost.filter((post: any) => post.type == selectedType);
+
   return (
-    <div>
-      <p>환영합니다, {session.user.email}!</p>
+    <>
+      
+    <div className="flex w-full ">
+      <div className="flex flex-col w-36 bg-yellow-100 fixed basis-1">      
+        {[...types].map((type: any) => (
+          <div key={type} className="h-36 w-full bg-gray-100 mt-4" onClick={() => setSelectedType(type)}>{type}</div>
+        ))}
+
+      </div>
+      <div className="flex flex-col items-center w-full justify-center">
+      {filteredPosts.map((post: any) => (
+        <div key={post.id} className=" rounded overflow-hidden shadow-md m-4 w-3/4">
+        {/* <img className="w-full" src="/path-to-your-image.jpg" alt="Post Image"/> */}
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{post.title}</div>
+          <div
+            className="text-gray-700 text-base"
+            dangerouslySetInnerHTML={{ __html: marked.parse(post.content || '') }}
+          />
+        </div>
+      </div>
+      ))}
+      </div>
     </div>
+    </>
   );
 }

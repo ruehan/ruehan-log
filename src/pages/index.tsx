ea4b3fbe38506eb96f9f464de2fcf9d269611553
vendredl <node.js_@kakao.com>
2023-11-22 19:20:11 +0900

    :lipstick: 목차에 framer-motion 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { marked } from 'marked';
+import {motion} from 'framer-motion'
 
 const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
@@ -31,17 +32,42 @@ export default function Home() {
 
   const types = new Set(posts.getPost.map((post: any) => post.type));
   const filteredPosts = posts.getPost.filter((post: any) => post.type == selectedType);
+  
+  const itemVariants = {
+    hidden: { scale: 0, rotate: 0 },
+    visible: {
+      scale: 1,
+      rotate: 360,
+      transition: {
+        type: "tween",
+        stiffness: 260,
+        damping: 20,
+        duration: .75
+      }
+    }
+  };
 
   return (
     <>
       
     <div className="flex w-full ">
-      <div className="flex flex-col w-36 bg-yellow-100 fixed basis-1">      
+      <motion.div 
+        className="flex flex-col w-36  fixed basis-1 items-center">      
         {[...types].map((type: any) => (
-          <div key={type} className="h-36 w-full bg-gray-100 mt-4" onClick={() => setSelectedType(type)}>{type}</div>
+          <motion.div 
+            key={type} 
+            className="h-36 w-32 bg-gray-100 mt-4 bg-white text-center  shadow-lg border rounded-md flex items-center justify-center" 
+            onClick={() => setSelectedType(type)}
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.95}}
+            variants={itemVariants}
+            initial="hidden"
+            animate="visible"
+            >{type}
+          </motion.div>
         ))}
 
-      </div>
+      </motion.div>
       <div className="flex flex-col items-center w-full justify-center">
       {filteredPosts.map((post: any) => (
         <div key={post.id} className=" rounded overflow-hidden shadow-md m-4 w-3/4">

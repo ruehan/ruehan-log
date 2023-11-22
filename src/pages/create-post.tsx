81de488f3a726f13b2a15e6eb1f14319dc0ce8bd
vendredl <node.js_@kakao.com>
2023-11-22 18:46:35 +0900

    :lipstick: Post 생성 후 메인화면으로 이동하도록 수정

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { marked } from 'marked';
 import DragAndDropUpload from './components/DragAndDrop';
+import { useRouter } from 'next/router';
 
 
 marked.setOptions({
@@ -11,9 +12,10 @@ marked.setOptions({
   });
 
 export default function PostEditor() {
-  const { register, watch, setValue } = useForm();
+  const { register, watch, setValue, handleSubmit } = useForm();
   const [imageVariants, setImageVariants] = useState("");
   let markdown = watch('markdown');
+  const router = useRouter();
 
   const handleImageUpload = (variants: any) => {
     setImageVariants(variants);
@@ -24,10 +26,35 @@ export default function PostEditor() {
     setValue('markdown', markdown + imageMarkdown);
   };
 
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await fetch('/api/create-post', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error('회원가입 실패');
+      }
+
+      // 회원가입 성공 처리
+      const result = await response.json();
+      console.log(result); // 또는 사용자에게 성공 메시지 표시
+      router.push('/')
+    } catch (error) {
+      console.error('회원가입 중 에러 발생:', error);
+      // 사용자에게 에러 메시지 표시
+    }
+  };
+
   return (
     <div className="flex overflow-hidden h-screen -m-6"> 
       <div className="flex-1 h-screen w-1/2 bg-yellow-100">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <button type="submit" className="w-full h-12 p-8 bg-purple-100 flex justify-center items-center">포스트 생성</button>
           <input 
             {...register('title')}
             placeholder='Title'
@@ -42,6 +69,7 @@ export default function PostEditor() {
             {...register('markdown')}
             className="w-full h-screen p-8 overflow-scroll"
           />
+          
         </form>
         <DragAndDropUpload onImageUpload={handleImageUpload} />
       </div>

294e3851b369525d504b2a546bcbd60d26e25212
vendredl <node.js_@kakao.com>
2023-11-22 20:45:48 +0900

    :lipstick: Pagination 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { marked } from 'marked';
-import {motion} from 'framer-motion'
+import {motion, AnimatePresence} from 'framer-motion'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
@@ -12,6 +16,13 @@ export default function Home() {
   const router = useRouter();
   const { data: posts, error } = useSWR('/api/get-post', fetcher);
   const [selectedType, setSelectedType] = useState('테스트');
+  const [current, setCurrent] = useState(0);
+  const [direction, setDirection] = useState(0);
+
+  const paginate = (newIndex: any) => {
+    setCurrent(newIndex);
+    setDirection(newIndex > current ? 1 : 0);
+  }
 
   useEffect(() => {
     console.log(session)
@@ -19,6 +30,9 @@ export default function Home() {
       // signIn();
     }
   }, [session, status, router]);
+
+
+
 
   if (status === 'loading' || !session) {
     return <div>Loading...</div>;
@@ -28,11 +42,14 @@ export default function Home() {
     return <div>Loading...</div>
   }
 
+  
+
   console.log(posts)
 
   const types = new Set(posts.getPost.map((post: any) => post.type));
   const filteredPosts = posts.getPost.filter((post: any) => post.type == selectedType);
   
+
   const itemVariants = {
     hidden: { scale: 0, rotate: 0 },
     visible: {
@@ -47,12 +64,41 @@ export default function Home() {
     }
   };
 
+  const variants = {
+    enter: (direction: any) => {
+      return {
+        x: direction === 0 ? -1000 : 1000,
+        opacity: 0
+      };
+    },
+    center: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1
+    },
+    exit: (direction: any) => {
+      return {
+        zIndex: 0,
+        x: direction === 0 ? 1000 : -1000,
+        opacity: 0
+      };
+    }
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
   return (
     <>
       
-    <div className="flex w-full ">
+    <div className="flex w-full items-center justify-center bg-red-100" style={{height: '100vh'}}>
       <motion.div 
-        className="flex flex-col w-36  fixed basis-1 items-center">      
+        className="flex flex-col w-36 items-center bg-yellow-100">      
         {[...types].map((type: any) => (
           <motion.div 
             key={type} 
@@ -68,21 +114,44 @@ export default function Home() {
         ))}
 
       </motion.div>
-      <div className="flex flex-col items-center w-full justify-center">
-      {filteredPosts.map((post: any) => (
-        <div key={post.id} className=" rounded overflow-hidden shadow-md m-4 w-3/4">
-        {/* <img className="w-full" src="/path-to-your-image.jpg" alt="Post Image"/> */}
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{post.title}</div>
-          <div
-            className="text-gray-700 text-base"
-            dangerouslySetInnerHTML={{ __html: marked.parse(post.content || '') }}
-          />
+  
+      <div className="flex w-5/6 justify-center h-3/4 bg-blue-100 overflow-hidden">
+        <div className="relative bg-white overflow-scroll w-3/4 flex flex-col justify-between items-center">
+          <AnimatePresence initial={false}>
+            <motion.div
+              key={current}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 }
+              }}>
+                {
+                  <div key={filteredPosts[current].id} className="">
+                    <div className="px-6 py-4">
+                      <div className="font-bold text-xl mb-2">{filteredPosts[current].title}</div>
+                      <div
+                        className="text-gray-700 text-base"
+                        dangerouslySetInnerHTML={{ __html: marked.parse(filteredPosts[current].content || '') }}
+                      />
+                    </div>
+                  </div>
+                }
+
+            </motion.div>
+          </AnimatePresence>
+          <div className="w-3/4 flex justify-around fixed bottom-5 ">
+            <button onClick={() => 
+              paginate((current - 1 + filteredPosts.length) % filteredPosts.length)}>이전</button>
+            {current + 1}
+            <button onClick={() => paginate((current + 1) % filteredPosts.length)}>다음</button>
+          </div>
         </div>
       </div>
-      ))}
       </div>
-    </div>
     </>
   );
 }

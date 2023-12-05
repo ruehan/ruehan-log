3e1b94d22172e2f8f4caac7d9b56ef0e3d8fa5fc
vendredl <node.js_@kakao.com>
2023-12-05 16:04:34 +0900

    :alien: Edit-post API 추가

---
+++ src/pages/api/edit-post.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { hash } from 'bcryptjs';
+import prisma from '@/utils/prisma';
+import { marked } from 'marked';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { title, type, content, markdown, postId } = req.body;
+
+    console.log(postId)
+
+    try {
+        const userId = 1; // 로그인한 사용자의 ID
+
+        const newPost = await prisma.post.update({
+          where: {
+            id: parseInt(postId)
+          },
+          data: {
+            title: title,
+            content: markdown,
+            type: type,
+            authorId: userId,
+          },
+        });
+
+        console.log(newPost)
+
+      res.status(201).json({ message: '포스트가 수정 되었습니다.', newPost });
+    } catch (error) {
+      res.status(500).json({ message: '포스트 수정 중 에러가 발생했습니다.' });
+    }
+  } else {
+    // POST 요청이 아닌 경우 에러 처리
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+  }
+}

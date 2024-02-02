14a28a6cfba0e5b98a2192aa676de92cf9855c58
vendredl <node.js_@kakao.com>
2024-02-02 16:04:13 +0900

    :heavy_plus_sign: SWRConfig 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr/_internal'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => 
+          fetch(url).then((response) => response.json())
+      }}>
+      <SessionProvider>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </SWRConfig>
+  )
+  
 }

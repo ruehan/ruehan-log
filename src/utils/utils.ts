09b88dd5af3d583532946c9c4e447e20818e7f0c
vendredl <node.js_@kakao.com>
2024-02-06 18:35:47 +0900

    :speech_balloon: 새 업데이트 확인을 위한 함수 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,21 @@
 import moment from "moment";
+import { v4 as uuidv4 } from 'uuid';
+
+export function generateToken() {
+  const token = uuidv4();
+  return token;
+}
+
 
 export const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export function unix_timestamp(t: moment.MomentInput){  
     return moment(t).format('YYYY-MM-DD HH:mm:ss')
   }
+
+export function isUpdated(t: moment.MomentInput){
+  const inputTime = moment(t);
+  const updatedTime = moment().subtract(7, 'days');
+
+  return inputTime.isAfter(updatedTime)
+}

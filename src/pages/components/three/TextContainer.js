1aa0203ed54c05c8f10c2db3bb63987071e22e21
vendredl <node.js_@kakao.com>
2024-03-05 17:38:23 +0900

    :speech_balloon: Text Box 추가

---
+++ src/pages/components/three/TextContainer.js
@@ -0,0 +1,11 @@
+import { Text } from '@react-three/drei';
+
+function TextContainer() {
+  return (
+    <Text position={[0, 1, 50]} fontSize={1.4}>
+      세계 여행
+    </Text>
+  );
+}
+
+export default TextContainer;
